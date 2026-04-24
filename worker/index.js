9e6a9fec501e164560285f2b12ff76dd61e52b11
ahmadlatifdev <privategate777@gmail.com>
2026-04-24 00:10:11 -0400

    force update worker

--- worker/index.js
+++ worker/index.js
@@ -1,1 +1,2 @@
 const Sentry = require('@sentry/node'); Sentry.init({ dsn: 'https://216ffa4485f88b8250100eee059110b5@o4511260643229696.ingest.us.sentry.io/4511260644474880' }); async function run(){ Sentry.captureException(new Error('SENTRY FINAL DIRECT DSN')); await Sentry.flush(5000); console.log('FLUSH DONE'); setInterval(()=>console.log('alive'),60000); } run();
+//force update
